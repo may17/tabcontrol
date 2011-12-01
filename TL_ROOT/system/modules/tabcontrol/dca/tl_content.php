f5b7cc3fbbdd6581d8d15c7d8933fd428a2a7bee
Christian Barkowsky <97media@Christian-Barkowskys-MacBook-Pro.local>
2011-12-01 11:52:25 +0100

    xxx

--- TL_ROOT/system/modules/tabcontrol/dca/tl_content.php
+++ TL_ROOT/system/modules/tabcontrol/dca/tl_content.php
@@ -7,21 +7,21 @@
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation, either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this program. If not, please visit the Free
  * Software Foundation website at http://www.gnu.org/licenses/.
  *
  * PHP version 5
- * @copyright  Jean-Bernard Valentaten 2009 
- * @author     Jean-Bernard Valentaten <troggy.brains@gmx.de> 
- * @package    TabControl 
- * @license    GNU/LGPL 
+ * @copyright  Jean-Bernard Valentaten 2009
+ * @author     Jean-Bernard Valentaten <troggy.brains@gmx.de>
+ * @package    TabControl
+ * @license    GNU/LGPL
  * @filesource
  */
 
@@ -33,7 +33,7 @@
 // Palettes
 $GLOBALS['TL_DCA']['tl_content']['palettes']['__selector__'][] = 'tabType';
 $GLOBALS['TL_DCA']['tl_content']['palettes']['tabcontrol'] = '{type_legend},type,tabType';
-$GLOBALS['TL_DCA']['tl_content']['palettes']['tabcontroltab'] = '{type_legend},type,tabType;{tab_legend},tabTitles,tabBehaviour,tabClasses;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
+$GLOBALS['TL_DCA']['tl_content']['palettes']['tabcontroltab'] = '{type_legend},type,headline,tabType;{tab_legend},tabTitles,tabBehaviour,tabClasses;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
 $GLOBALS['TL_DCA']['tl_content']['palettes']['tabcontrolstart'] = '{type_legend},type,tabType;{tab_legend},tabClasses;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
 $GLOBALS['TL_DCA']['tl_content']['palettes']['tabcontrolstop'] = '{type_legend},type,tabType;{protected_legend:hide},protected;{expert_legend:hide},guests';
 
